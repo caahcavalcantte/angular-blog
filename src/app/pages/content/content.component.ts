106c7af6c6852498ee7bcf26136bee5855e092e0
caahcavalcantte <cacavalcantte@gmail.com>
2023-09-15 11:23:25 -0300

    criando outras páginas

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-
+  photoCover:string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSstQ43Dl5wkIqN4GZ0tnW_aZVFVqvZ6iMRqA&usqp=CAU"
+  contentTitle:string = "Minha Noticia"
+  contentDescription:string ="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, autem."
 }
